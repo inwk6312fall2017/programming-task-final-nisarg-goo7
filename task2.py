9485a1f1614559f563e4e1fe9e7ea3ee92aa5f22
singhbikramjit <bikramjit.singh@dal.ca>
2017-10-20 15:21:33 -0300

    still working on task 2

--- task2.py
+++ task2.py
@@ -1,15 +1,7 @@
-import unittest
+
 from weather import Weather
-weather=Weather()
-location = weather.lookup_by_location("Halifax")
-condition = location.condition()
-temp=condition.get('temp')
-forecast=location.forecast()
-print (condition['text'])
-
-# Get weather forecasts for the upcoming days.
-for forecasts in location.forecast():
-    print (forecasts['text'])
-    print (forecasts['date'])
-    print (forecasts['high'])
-
+weather = Weather()
+def lookuplocation(city):
+    location = weather.lookup_by_location(city)
+    print(location)
+    condition = location.condition() 
