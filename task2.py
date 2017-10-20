b5e7ef24e4d15ad534d4f6907a7084b7a688898a
singhbikramjit <bikramjit.singh@dal.ca>
2017-10-20 15:39:33 -0300

    max temp succssful

--- task2.py
+++ task2.py
@@ -1,7 +1,15 @@
-
 from weather import Weather
 weather = Weather()
-def lookuplocation(city):
-    location = weather.lookup_by_location(city)
-    print(location)
-    condition = location.condition() 
+#def lookuplocation(city):
+location = weather.lookup_by_location('Halifax')
+condition = location.condition()
+temp=[]
+forecasts = location.forecast()
+for i in forecasts:
+		high=i['high']
+		temp.append(int(high))
+#	for i in forecasts:
+		if int(i['high'])==max(temp):
+			print('Day with max tep in next 5 days is %s '% (i['date']))
+			print('higest temprature is %s'%(i['high']))
+			break
